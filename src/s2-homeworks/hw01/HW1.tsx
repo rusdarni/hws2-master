6a24bdd325f26dcb3605825ec014501b9dba3bc0
dasha <rus.dar.ni@gmail.com>
2026-08-03 22:10:41 +0300

    second commit

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,7 +14,21 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type UserType = {
+    avatar: string,
+    name: string
+}
+
+export type MessageTextType = {
+    text: string,
+    time: string
+}
+
+export type MessageType = {
+    id: number,
+    user: UserType,
+    message: MessageTextType
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
