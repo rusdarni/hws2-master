8c66d26e7631bdec29e7848b3dc44b4088cdd7fd
dasha <rus.dar.ni@gmail.com>
2026-08-14 22:28:55 +0300

    11 commit

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -31,7 +31,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={(props) => props.isActive ? s.active : ''}// делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,6 +39,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
+                        className={(props) => props.isActive ? s.active : ''}
                         // className={...} // делает студент
                     >
                         Junior
@@ -47,6 +48,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
+                        className={(props) => props.isActive ? s.active : ''}
                         // className={...} // делает студент
                     >
                         Junior Plus
