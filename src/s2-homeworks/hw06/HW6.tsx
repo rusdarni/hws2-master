fbee8d8dea43e65808ac1e7dca0dffc8a600954b
dasha <rus.dar.ni@gmail.com>
2026-08-23 20:18:17 +0300

    12 commit

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -18,8 +18,9 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
+        const savedValue = restoreState<string>('hw6-editable-span-value', '')
+        setValue(savedValue)
         // делают студенты
-
     }
 
     return (
