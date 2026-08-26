3dbe4ffd86f9fbf7d1b60935dd6f75ff88914362
dasha <rus.dar.ni@gmail.com>
2026-08-26 22:53:03 +0300

    14 commit

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            if (action.payload === 'up'){
+                return [...state].sort((a, b) => a.name.charCodeAt(0) - b.name.charCodeAt(0))
+            }
+            else return [...state].sort((a, b) => b.name.charCodeAt(0) - a.name.charCodeAt(0))
 
-            return state // need to fix
+
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(student => student.age > 18) // need to fix
         }
         default:
             return state
